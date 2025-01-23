7e9031a966aa66a092af4c2bc81ba368b69d30e2
Hasturious <jbster559@gmail.com>
2025-01-22 18:14:48 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name?: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("What is your last name?: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("How old are you?: ");
+            userAge = int.Console.ReadLine();
+
+            Console.Write("What is the price of the item you're interested in?: ");
+            itemPrice = double.Console.ReadLine();
+
+            Console.Write("Are you a student?: ");
+            isStudent = bool.Console.ReadLine();
+
+
+        }
+    }
+}
