f53925473f5fcdc78080558efa47ae6786c4e9c6
Hasturious <jbster559@gmail.com>
2025-01-22 19:23:14 -0800

    Added documentation

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -15,40 +15,38 @@ namespace COMP003A.Assignment2
             double itemPrice; //store the product price
             bool isStudent; //true / false statement
 
-            string asteriskLine = "************************************************************";
+            string asteriskLine = "************************************************************"; //Stores the string for the large asterisk line
 
-            Console.WriteLine(asteriskLine+"\n Welcome to the Discount Calculator"+"\n"+asteriskLine);
+            Console.WriteLine(asteriskLine+"\n Welcome to the Discount Calculator"+"\n"+asteriskLine); //Displays title message
 
-            Console.Write("What is your first name?: ");
-            firstName = Console.ReadLine();
+            Console.Write("What is your first name?: "); //prompts user for first name
+            firstName = Console.ReadLine(); //logs the first name
 
-            Console.Write("What is your last name?: ");
-            lastName = Console.ReadLine();
+            Console.Write("What is your last name?: "); //prompt user for last name
+            lastName = Console.ReadLine(); //logs last name
 
-            Console.Write("How old are you?: ");
-            userAge = int.Parse(Console.ReadLine());
+            Console.Write("How old are you?: "); //prompts user for age
+            userAge = int.Parse(Console.ReadLine()); //logs age as an int
 
-            Console.Write("What is the price of the item you're interested in?: ");
-            itemPrice = double.Parse(Console.ReadLine());
+            Console.Write("What is the price of the item you're interested in?: "); //prompt the price of desired item
+            itemPrice = double.Parse(Console.ReadLine()); //prompts the price as an double
 
-            Console.Write("Are you a student? (true/false): ");
-            isStudent = bool.Parse(Console.ReadLine());
+            Console.Write("Are you a student? (true/false): "); //prompts user if their a student
+            isStudent = bool.Parse(Console.ReadLine()); //logs result an a boolean
 
-            int futureAge = userAge + 5; // adds 5 to user age
-            bool isAdult = userAge >= 18; //checks if adult
+            int futureAge = userAge + 5; //adds 5 to user age
             bool isSenior = userAge >= 60; //checks if senior
-            double studentDiscount = itemPrice * .9;
-            double seniorDiscount = itemPrice * .8;
-            
-            Console.WriteLine("\n\n\n"+asteriskLine);
+            double studentDiscount = itemPrice * .9; //discounts the price by 10%
+            double seniorDiscount = itemPrice * .8; //discounts the price by 20%
+
+            Console.WriteLine("\n\n\n"+asteriskLine); //go down 3 line spaces and displays the asterisk line
             Console.WriteLine($"Hello {firstName} {lastName}!"); //displays name
             Console.WriteLine($"You are current age {userAge} years old."); //displays age
-            Console.WriteLine($"In five years you will be {futureAge}.");
-            Console.WriteLine($"Are you 18 or older {isAdult}."); //displays if adult
-            Console.WriteLine($"The original price of the item is {itemPrice}.");
-            Console.WriteLine($"As a student, your discounted price is {studentDiscount}.00.");
-            Console.WriteLine($"As a student, your discounted price is {seniorDiscount}.00.");
-            Console.WriteLine(asteriskLine);
+            Console.WriteLine($"In five years you will be {futureAge}."); //displays future age
+            Console.WriteLine($"The original price of the item is {itemPrice}."); //displays original price
+            Console.WriteLine($"As a student, your discounted price is {studentDiscount}.00."); //displays student discount
+            Console.WriteLine($"As a student, your discounted price is {seniorDiscount}.00."); //displays senior discount
+            Console.WriteLine(asteriskLine); //displays asterisk line
         }
     }
 }
