76bd43b016eabe4b7be4c960bb784003a48da802
Hasturious <jbster559@gmail.com>
2025-01-22 18:20:12 -0800

    Added variable names

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            string firstName; //store name as a string
+            string lastName; //store name as a string
+            int userAge; //store age as an number
+            double itemPrice; //store the product price
+            bool isStudent; //true / false statement
+
             Console.Write("What is your first name?: ");
             firstName = Console.ReadLine();
 
@@ -19,7 +25,15 @@
             Console.Write("Are you a student?: ");
             isStudent = bool.Console.ReadLine();
 
+            int futureAge = userAge + 5; // adds 5 to user age
+            bool isAdult = userAge >= 18; //checks if adult
+            bool isAdultStudent = isStudent && isAdult; //checks if both student and adult
 
+            Console.WriteLine($"\nHello {userName}!"); //displays name
+            Console.WriteLine($"You are current age {userAge} years old."); //displays age
+            Console.WriteLine("In five years you will be {futureAge}");
+            Console.WriteLine($"Are you 18 or older {isAdult}"); //displays if adult
+            Console.WriteLine($"Are you both a student and 18 or older? {isAdultStudent}"); //displays if adult and student
         }
     }
 }
